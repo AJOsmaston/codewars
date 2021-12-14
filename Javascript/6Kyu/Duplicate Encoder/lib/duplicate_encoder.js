8b1347229ad2fda969bc080f7fe3c66d156ccd94
AJOsmaston <dajoxx@gmail.com>
2021-12-14 12:28:25 +0000

    extracted two additional methods

--- Javascript/6Kyu/Duplicate Encoder/lib/duplicate_encoder.js
+++ Javascript/6Kyu/Duplicate Encoder/lib/duplicate_encoder.js
@@ -1,14 +1,23 @@
 const duplicateEncoder = (input) => {
     let result = "";
-    let scoring = {};
-    let standardised_array = input.split("").map(letter => letter.toLowerCase())
-    standardised_array.forEach(letter => {
-        !scoring[letter] ? scoring[letter] = 1 : scoring[letter] += 1;
-    });
-    standardised_array.forEach(letter => {
+    let standardisedArray = convert(input)
+    let scoring = calculateScores(standardisedArray)
+    standardisedArray.forEach(letter => {
         result += (scoring[letter] === 1 ? "(" : ")");
     });
     return result;
+}
+
+const convert = (input) => {
+    return input.split("").map(letter => letter.toLowerCase())
+}
+
+const calculateScores = (array) => {
+    let scoring = {};
+    array.forEach(letter => {
+        !scoring[letter] ? scoring[letter] = 1 : scoring[letter] += 1;
+    });
+    return scoring;
 }
 
 module.exports = duplicateEncoder;
