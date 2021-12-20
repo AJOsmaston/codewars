787ddf3fe41611c078fbac3bf57b21804fe78dd0
AJOsmaston <dajoxx@gmail.com>
2021-12-20 10:51:24 +0000

    different example tests for same cases

--- Ruby/6Kyu/PersistentBugger/spec/persistence_spec.rb
+++ Ruby/6Kyu/PersistentBugger/spec/persistence_spec.rb
@@ -1,12 +1,27 @@
 require 'persistence'
 
 describe 'persistence' do
-  it 'calculates for single digit' do
-    expect(persistence(5)).to eq 5
+  describe 'single step' do
+    describe 'calculates for single digit' do
+      it 'calculates for 5' do
+        expect(persistence(5)).to eq 5
+      end
+  
+      it 'calculates for 9' do
+        expect(persistence(9)).to eq 9
+      end
+    end
+  
+    describe 'calculates for double digits' do
+      it 'calculates for 19' do
+        expect(persistence(19)).to eq 9
+      end  
+  
+      it 'calculates for 14' do
+        expect(persistence(14)).to eq 4
+      end
+    end
   end
-
-  it 'calculates for double digits' do
-    expect(persistence(19)).to eq 9
-  end
+  
 
 end
