804b7380627dfae443efac3b79fee69ced1cca04
AJOsmaston <dajoxx@gmail.com>
2021-12-20 11:05:39 +0000

    realised misunderstood brief. Changed tests to reflect actual cases.

--- Ruby/6Kyu/PersistentBugger/lib/persistence.rb
+++ Ruby/6Kyu/PersistentBugger/lib/persistence.rb
@@ -1,3 +1,10 @@
 def persistence(num)
-  num.to_s.split('').map(&:to_i).inject(:*)
+  count = 0
+  loop do
+    break if num.to_s.length == 1
+    num = num.to_s.split('').map(&:to_i).inject(:*)
+    p num
+    count += 1
+  end
+  count
 end
--- Ruby/6Kyu/PersistentBugger/spec/persistence_spec.rb
+++ Ruby/6Kyu/PersistentBugger/spec/persistence_spec.rb
@@ -1,27 +1,41 @@
 require 'persistence'
 
 describe 'persistence' do
-  describe 'single step' do
+  describe '0 step' do
     describe 'calculates for single digit' do
       it 'calculates for 5' do
-        expect(persistence(5)).to eq 5
+        expect(persistence(5)).to eq 0
       end
   
       it 'calculates for 9' do
-        expect(persistence(9)).to eq 9
+        expect(persistence(9)).to eq 0
       end
-    end
-  
-    describe 'calculates for double digits' do
-      it 'calculates for 19' do
-        expect(persistence(19)).to eq 9
-      end  
-  
-      it 'calculates for 14' do
-        expect(persistence(14)).to eq 4
+
+      it 'calculates for 4' do
+        expect(persistence(4)).to eq 0
       end
     end
   end
   
+  describe 'single step' do
+    describe 'calculates for double digits' do
+      it 'calculates for 19' do
+        expect(persistence(19)).to eq 1
+      end  
+  
+      it 'calculates for 14' do
+        expect(persistence(14)).to eq 1
+      end
+    end
+  end
 
+  describe 'multiple step' do
+    it 'calculates for 56' do
+      expect(persistence(56)).to eq 2
+    end
+
+    it 'calculates for 999' do
+      expect(persistence(999)).to eq 4
+    end
+  end
 end
