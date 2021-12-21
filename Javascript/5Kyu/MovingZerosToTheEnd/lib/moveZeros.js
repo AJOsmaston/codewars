596d27952a183f2c25de1b498a0e071b043792bb
AJOsmaston <dajoxx@gmail.com>
2021-12-21 18:55:08 +0000

    moves a 0 to the end of an array of numbers

--- Javascript/5Kyu/MovingZerosToTheEnd/lib/moveZeros.js
+++ Javascript/5Kyu/MovingZerosToTheEnd/lib/moveZeros.js
@@ -1,5 +1,16 @@
 const moveZeros = (array) => {
-  return array
+
+  result = []
+
+  for (var i = array.length - 1; i >= 0; i--) {
+    if(array[i] === 0){
+      result.push(array[i])
+    }
+    else {
+      result.unshift(array[i])
+    }
+  }
+    return result
 }
 
 module.exports = moveZeros;
--- Javascript/5Kyu/MovingZerosToTheEnd/test/moveZeros.test.js
+++ Javascript/5Kyu/MovingZerosToTheEnd/test/moveZeros.test.js
@@ -4,4 +4,8 @@ describe('moveZeros', () => {
   it("doesn't move non 0's from a list of numbers", () => {
     expect(moveZeros([1,2,3])).toEqual([1,2,3])
   });
+
+  it("moves a 0 inside a list of numbers", () => {
+    expect(moveZeros([1,0,2,3])).toEqual([1,2,3,0])
+  });
 });
