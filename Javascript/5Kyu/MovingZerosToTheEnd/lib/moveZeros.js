5a21932d99d3e1a795a333cf42f78a5636441a66
AJOsmaston <dajoxx@gmail.com>
2021-12-21 18:58:10 +0000

    works for example

--- Javascript/5Kyu/MovingZerosToTheEnd/lib/moveZeros.js
+++ Javascript/5Kyu/MovingZerosToTheEnd/lib/moveZeros.js
@@ -1,16 +1,16 @@
 const moveZeros = (array) => {
 
-  result = []
+  result = [];
 
   for (var i = array.length - 1; i >= 0; i--) {
     if(array[i] === 0){
-      result.push(array[i])
+      result.push(array[i]);
     }
     else {
-      result.unshift(array[i])
-    }
-  }
-    return result
-}
+      result.unshift(array[i]);
+    };
+  };
+    return result;
+};
 
 module.exports = moveZeros;
--- Javascript/5Kyu/MovingZerosToTheEnd/test/moveZeros.test.js
+++ Javascript/5Kyu/MovingZerosToTheEnd/test/moveZeros.test.js
@@ -1,11 +1,20 @@
 const moveZeros = require("../lib/moveZeros");
 
 describe('moveZeros', () => {
-  it("doesn't move non 0's from a list of numbers", () => {
-    expect(moveZeros([1,2,3])).toEqual([1,2,3])
+  describe('numbers', () => {
+    it("doesn't move non 0's from a list of numbers", () => {
+      expect(moveZeros([1,2,3])).toEqual([1,2,3])
+    });
+
+    it("moves a 0 inside a list of numbers", () => {
+      expect(moveZeros([1,0,2,3])).toEqual([1,2,3,0])
+    });
   });
 
-  it("moves a 0 inside a list of numbers", () => {
-    expect(moveZeros([1,0,2,3])).toEqual([1,2,3,0])
+  describe('example', () => {
+    it('runs the example', () => {
+      expect(moveZeros([false,1,0,1,2,0,1,3,"a"]))
+        .toEqual([false,1,1,2,1,3,"a",0,0])
+    });
   });
 });
