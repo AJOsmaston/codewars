df53118dcdc79a6ef30486417fe538d9852b7d24
AJOsmaston <dajoxx@gmail.com>
2021-12-23 14:14:40 +0000

    extract format function

--- Javascript/5Kyu/HumanReadableTime/lib/humanReadable.js
+++ Javascript/5Kyu/HumanReadableTime/lib/humanReadable.js
@@ -1,17 +1,15 @@
 function humanReadable (seconds) {
   let ss = (seconds % 60)
-    .toString()
-    .padStart(2, '0');
 
   let mm = (Math.floor(seconds / 60) % 60)
-    .toString()
-    .padStart(2, '0');
 
   let hh = (Math.floor(seconds / 3600) % 100)
-    .toString()
-    .padStart(2, '0');
 
-  return `${hh}:${mm}:${ss}`;
+  return `${format(hh)}:${format(mm)}:${format(ss)}`;
+}
+
+const format = (input) => {
+  return input.toString().padStart(2, '0');
 }
 
 module.exports = humanReadable;
