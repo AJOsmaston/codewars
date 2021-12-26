3341909934ceef71ae9cca0753165e2357bad41d
AJOsmaston <dajoxx@gmail.com>
2021-12-26 14:05:53 +0000

    more day tests

--- Javascript/4Kyu/HumanReadableDurationFormat/test/formatDuration.test.js
+++ Javascript/4Kyu/HumanReadableDurationFormat/test/formatDuration.test.js
@@ -48,6 +48,22 @@ describe('tests', () => {
     it('calculates for 86400s', () => {
       expect(formatDuration(86400)).toEqual("1 day");
     });
+
+    it('calculates for 172800s', () => {
+      expect(formatDuration(172800)).toEqual("2 days");
+    });
+
+    it('calculates for 172801s', () => {
+      expect(formatDuration(172801)).toEqual("2 days and 1 second");
+    });
+
+    it('calculates for 172861s', () => {
+      expect(formatDuration(172861)).toEqual("2 days, 1 minute and 1 second");
+    });
+
+    it('calculates for 176461s', () => {
+      expect(formatDuration(176461)).toEqual("2 days, 1 hour, 1 minute and 1 second");
+    });
   });
   
 });
