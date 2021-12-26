aab69c39127b000a9235c72f4738a75f639f8d84
AJOsmaston <dajoxx@gmail.com>
2021-12-26 13:23:49 +0000

    61 seconds

--- Javascript/4Kyu/HumanReadableDurationFormat/lib/formatDuration.js
+++ Javascript/4Kyu/HumanReadableDurationFormat/lib/formatDuration.js
@@ -1,6 +1,22 @@
 const formatDuration = (s) => {
-  if (s == 1) { return `${s} second` }
-  else { return `${s} seconds` }
+  result = ""
+
+  if ( s > 60 ) {
+    minutes = Math.floor(s/60)
+    result += pluralise(minutes, "minute") + " and "
+    s -= (minutes * 60)
+  }
+
+  result += pluralise(s, "second")
+
+  return result;
 };
+
+const pluralise = (input, time) => {
+  if ( input >= 1 ) {
+    if (input == 1) { return `${input} ${time}`; }
+    else { return `${input} ${time}s`; };
+  }
+}
 
 module.exports = formatDuration;
--- Javascript/4Kyu/HumanReadableDurationFormat/test/formatDuration.test.js
+++ Javascript/4Kyu/HumanReadableDurationFormat/test/formatDuration.test.js
@@ -1,11 +1,20 @@
 const formatDuration = require('../lib/formatDuration')
 
 describe('tests', () => {
-  it('calculates for 1s', () => {
-    expect(formatDuration(1)).toEqual("1 second")
-  })
+  describe('seconds', () => {
+    it('calculates for 1s', () => {
+      expect(formatDuration(1)).toEqual("1 second");
+    });
+  
+    it('calculates for 2s', () => {
+      expect(formatDuration(2)).toEqual("2 seconds");
+    });
+  });
 
-  it('calculates for 2s', () => {
-    expect(formatDuration(2)).toEqual("2 seconds")
-  })
-})
+  describe('minutes', () => {
+    it('calculates for 61s', () => {
+      expect(formatDuration(61)).toEqual("1 minute and 1 second");
+    });
+  });
+  
+});
