8a1ae914124a0d7ece42bc233e90699c5900e759
AJOsmaston <dajoxx@gmail.com>
2021-12-26 11:17:49 +0000

    returns null

--- Javascript/6Kyu/GiveMeADiamond/lib/diamond.js
+++ Javascript/6Kyu/GiveMeADiamond/lib/diamond.js
@@ -1,5 +1,5 @@
 const diamond = () => {
-
+  return null
 }
 
 module.exports = diamond;
--- Javascript/6Kyu/GiveMeADiamond/test/diamond.test.js
+++ Javascript/6Kyu/GiveMeADiamond/test/diamond.test.js
@@ -1,5 +1,10 @@
 const diamond = require('../lib/diamond')
 
-define('tests', () => {
+describe('tests', () => {
+  describe('edge cases', () => {
+    it('returns null on even 2', () => {
+      expect(diamond(2)).toEqual(null)
+    })
+  })
   
 })
