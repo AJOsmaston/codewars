57737ce0bbce7ec20304c21c0dafc1457d08ea4e
AJOsmaston <dajoxx@gmail.com>
2021-12-30 13:33:08 +0000

    removed sortedWords array, everything done in for loop extracted sortByLetter function

--- Javascript/5Kyu/WhereMyAnagramsAt/lib/anagrams.js
+++ Javascript/5Kyu/WhereMyAnagramsAt/lib/anagrams.js
@@ -1,27 +1,17 @@
 const anagrams = (word, words) => {
- 
-  
   let results = [];
 
-  let sortedWord = word.split('').sort().join('');
-  let sortedWords = [];
-
-  words.forEach(element => {
-    sortedWords.push(element.split('').sort().join(''));
-  });
-
-  if (!sortedWords.includes(sortedWord)) {
-    return [];
-  };
-
-  for(let i = 0; i < sortedWords.length; i++) {
-    if (sortedWords[i] === sortedWord) {
+  for(let i = 0; i < words.length; i++) {
+    if (sortByLetter(words[i]) === sortByLetter(word)) {
       results.push(words[i]);
     };
   };
 
   return results;
-
 };
+
+const sortByLetter = (input) => {
+  return input.split('').sort().join('');
+}
 
 module.exports = anagrams;
