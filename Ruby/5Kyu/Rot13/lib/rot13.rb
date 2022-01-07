19d846b0682d05bebef2e681aa9efb311737eefc
AJOsmaston <dajoxx@gmail.com>
2022-01-07 15:39:33 +0000

    works for end of alphabet, z

--- Ruby/5Kyu/Rot13/lib/rot13.rb
+++ Ruby/5Kyu/Rot13/lib/rot13.rb
@@ -1,6 +1,9 @@
 def rot13(input)
   13.times do
     input = input.next()
+    if input.length > 1
+      input = input[1]
+    end
   end
   input
 end
--- Ruby/5Kyu/Rot13/spec/rot13_spec.rb
+++ Ruby/5Kyu/Rot13/spec/rot13_spec.rb
@@ -1,7 +1,11 @@
 require 'rot13'
 
 describe 'rot 13' do
-  it 'runs the test' do
+  it 'works for a single character a' do
     expect(rot13('a')).to eq 'n'
+  end
+
+  it 'works for a single character z' do
+    expect(rot13('z')).to eq 'm'
   end
 end
