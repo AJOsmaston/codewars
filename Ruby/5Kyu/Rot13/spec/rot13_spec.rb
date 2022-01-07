8e667f557256375273a6a85ac0a91d6a96e4700d
AJOsmaston <dajoxx@gmail.com>
2022-01-07 15:40:50 +0000

    tests for uppercase single characters

--- Ruby/5Kyu/Rot13/spec/rot13_spec.rb
+++ Ruby/5Kyu/Rot13/spec/rot13_spec.rb
@@ -1,11 +1,22 @@
 require 'rot13'
 
 describe 'rot 13' do
-  it 'works for a single character a' do
-    expect(rot13('a')).to eq 'n'
-  end
+  describe 'single character' do
+    it 'works for a single character a' do
+      expect(rot13('a')).to eq 'n'
+    end
+  
+    it 'works for a single character z' do
+      expect(rot13('z')).to eq 'm'
+    end
 
-  it 'works for a single character z' do
-    expect(rot13('z')).to eq 'm'
+    it 'works for a single uppercase character A' do
+      expect(rot13('A')).to eq 'N'
+    end
+
+    it 'works for a single uppercase character A' do
+      expect(rot13('Z')).to eq 'M'
+    end
   end
+  
 end
