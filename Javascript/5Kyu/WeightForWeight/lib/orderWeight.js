c456d895ac826110aad92f89add811d24e23edc2
AJOsmaston <dajoxx@gmail.com>
2022-01-17 15:32:23 +0000

    unique numbers different weights works for 66 56

--- Javascript/5Kyu/WeightForWeight/lib/orderWeight.js
+++ Javascript/5Kyu/WeightForWeight/lib/orderWeight.js
@@ -1,5 +1,28 @@
 const orderWeight = (string) => {
-  return string
+  let weightArray = []
+  let weightMemo = {}
+  let resultArray = []
+
+  string.split(' ').forEach(number => {
+    // split into individual numbers
+    let numberArray = []
+    number.split('').forEach(character => {numberArray.push(parseInt(character))})
+    //sum the array (convert to weight)
+    let weight = numberArray.reduce((a, b) => a + b, 0)
+    weightArray.push(weight)
+    //memoize
+    weightMemo[weight] = number
+  })
+
+  //sort weights in ascending order
+  weightArray.sort((a, b) => { return a-b })
+
+  //replace weights with original value
+  weightArray.forEach(weight => {
+    resultArray.push(weightMemo[weight])
+  })
+
+  return resultArray.join(' ')
 }
 
 module.exports = orderWeight;
--- Javascript/5Kyu/WeightForWeight/test/orderWeight.test.js
+++ Javascript/5Kyu/WeightForWeight/test/orderWeight.test.js
@@ -4,4 +4,16 @@ describe('tests', () => {
   it('returns item as a string on individual item search', () => {
     expect(orderWeight("50")).toEqual("50")
   })
+
+  describe('sorted lists', () => {
+    describe('unique numbers different weights', () => {
+      it('65 56', () => {
+        expect(orderWeight("66 56")).toEqual("56 66")
+      })
+
+
+    })
+    
+  })
+  
 })
