28e5235b945ee443cfc1ba2fd6e28c22bc67f5f8
AJOsmaston <dajoxx@gmail.com>
2022-01-20 12:23:43 +0000

    checks for an additional item if avaialble

--- Javascript/5Kyu/PeteTheBaker/lib/cakes.js
+++ Javascript/5Kyu/PeteTheBaker/lib/cakes.js
@@ -1,9 +1,17 @@
 function cakes(recipe, available) {
+  let count = 0
+  let result = []
   for (const [recKey, recValue] of Object.entries(recipe)) {
     for (const [avaKey, avaValue] of Object.entries(available)) {
-      return avaValue / recValue
+      if (recKey === avaKey) { result.push(avaValue / recValue) }
     }
+    count += 1
   }
+
+  if (count !== result.length) { return 0 }
+  return Math.min(...result)
 }
+
+
 
 module.exports = cakes;
--- Javascript/5Kyu/PeteTheBaker/test/cakes.test.js
+++ Javascript/5Kyu/PeteTheBaker/test/cakes.test.js
@@ -1,23 +1,32 @@
 const cakes = require('../lib/cakes')
 
 describe('tests', () => {
-  it('checks for an apple', () => {
-    expect(cakes({apples: 1}, {apples: 1})).toEqual(1)
+  describe('single items', () => {
+    it('checks for an apple', () => {
+      expect(cakes({apples: 1}, {apples: 1})).toEqual(1)
+    })
+  
+    it('checks for a different quanitity of apples', () => {
+      expect(cakes({apples: 1}, {apples: 2})).toEqual(2)
+    })
+  
+    it('checks for a different quanitity of pears', () => {
+      expect(cakes({pears: 1}, {pears: 2})).toEqual(2)
+    })
+  
+    it('checks for a different quanitity of pears in recipe', () => {
+      expect(cakes({pears: 2}, {pears: 4})).toEqual(2)
+    })
+  
+    it('checks for a different quanitity of a dofferent item in recipe', () => {
+      expect(cakes({flour: 100}, {flour: 500})).toEqual(5)
+    })
+  })
+ 
+  describe('multiple items', () => {
+    it('checks for an additional available items', () => {
+      expect(cakes({apples: 1, pears: 1}, {apples: 1})).toEqual(0)
+    })
   })
 
-  it('checks for a different quanitity of apples', () => {
-    expect(cakes({apples: 1}, {apples: 2})).toEqual(2)
-  })
-
-  it('checks for a different quanitity of pears', () => {
-    expect(cakes({pears: 1}, {pears: 2})).toEqual(2)
-  })
-
-  it('checks for a different quanitity of pears in recipe', () => {
-    expect(cakes({pears: 2}, {pears: 4})).toEqual(2)
-  })
-
-  it('checks for a different quanitity of a dofferent item in recipe', () => {
-    expect(cakes({flour: 100}, {flour: 500})).toEqual(5)
-  })
 })
